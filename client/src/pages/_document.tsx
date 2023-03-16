0ca8973eadb9779372b523d8bf866d787aacda81
olo90632951 <olo90632951@gmail.com>
2023-03-16 18:13:23 +0900

    feat: meta 태그 추가

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <link rel="icon" href="/message.svg" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="누구나 소통할 수 있는 익명게시판 입니다." />
+        <meta name="keywords" content="익명, 게시판, 스마일 드래곤" />
+        <meta name="author" content="박근우" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
