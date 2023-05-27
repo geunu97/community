b6826f3c1fd070c88033d4b6c45f9cc13af9d45e
geunu97 <pgw@hanryubank.com>
2023-05-27 21:33:13 +0900

    feat: header, page 스토리 추가

---
+++ client/src/stories/organisms/Header/Header.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Header } from './Header';
+
+const meta: Meta<typeof Header> = {
+  title: 'Design System/Organisms/Header',
+  component: Header,
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {};
