bc313bed7f02ffc20d61b809de524a27d5a8753a
geunu97 <pgw@hanryubank.com>
2023-05-29 15:04:53 +0900

    remove: svg 파일 제거

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <link rel="icon" href="/message.svg" />
+        <link rel="icon" href="/svg/message.svg" />
         <meta charSet="UTF-8" />
         <meta name="description" content="누구나 소통할 수 있는 익명게시판 입니다." />
         <meta name="keywords" content="익명, 게시판, 스마일 드래곤" />
